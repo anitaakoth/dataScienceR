74d8ef6ee000e4ce04b5389925bd9adca6e1dc36
anita akoth <anitaakoth@gmail.com>
2019-03-19 00:23:20 +0300

    Data transformation  arrange() and select()

--- data_transformation.R
+++ data_transformation.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(nycflights13)
+
 #Exercise 5.2.4 Question 1
 filter(flights, (arr_delay >= 120 ))
 
@@ -14,7 +15,6 @@ filter(flights, (arr_delay > 120 | dep_delay <= 0 ))
 filter(flights, (dep_delay >= 120 & air_time >30 ))
 
 filter(flights, (dep_time >= 2400  | dep_time <=0600 ))
-
  
 #Question 2
 ##get values in a particular range
@@ -25,8 +25,41 @@ filter(flights, is.na(dep_time))
 ##Other missing variables dep_delay, arr_time ,  arr_delay
 #these are cancelled flights
 
-NA ^ 0
+#5.3.1 Exercises
+#Question 1
+arrange(flights, is.na(dep_time))
 
+#Question 2
+arrange(flights, desc(dep_delay))
+arrange(flights,  dep_delay)
+
+#Question 3
+arrange(flights,  air_time)
+
+#Question 4
+View(arrange(flights,   desc(air_time)))
+#longest: Flight 15
+View(arrange(flights,   air_time))
+
+#shortest: Flight 4368
+
+#5.4.1 Exercises
+
+#Question 1
+select(flights, dep_time, dep_delay, arr_time, arr_delay)
+select(flights, dep_time:arr_time)
+
+#Question 2
+select(flights, arr_time:arr_time)
+#Displays just the selected column
+
+#Question 3
+vars <- c("year", "month", "day", "dep_delay", "arr_delay")
+flights %>% select(one_of(vars)) 
+
+#Question 4
+select(flights, contains("TIME"))
+select(flights, time_hour, everything())
 
 
 
